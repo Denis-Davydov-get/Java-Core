a4ba79106010a613eef7a9a1b9cb556e05d9e581
Denis-Davydov-get <slim2001.88@mail.ru>
2024-10-30 18:11:32 +0300

    code edits

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 //        task1
         int[] arr = {2, 1, 2, 3, 4};
-        int[] arrZerro = {2, 0, 0, 3, 4};
+        int[] arrZero = {2, 0, 0, 3, 4};
         System.out.println("task1");
         System.out.println(task1.countEvens(arr));
 //      task2
@@ -12,6 +12,7 @@ public class Main {
         System.out.println(task2.minNumArray(arr));
         System.out.println(task2.maxNumArray(arr));
         System.out.println("task3");
-        System.out.println(task3.searchDoobleZero(arrZerro));
+        System.out.println(task3.searchDoobleZero(arr));
+        System.out.println(task3.searchDoobleZero(arrZero));
     }
 }
